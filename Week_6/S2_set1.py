619b5c158c704cf78965df3703889af5797619cc
camachoitzel <camachoitzel101@gmail.com>
2025-07-11 21:38:52 -0700

    Session 2 set 1 Problem work

--- Week_6/S2_set1.py
+++ Week_6/S2_set1.py
@@ -4,32 +4,73 @@
 # Note: a circular list is more than just a cycle - specifically connecting the first and last nodes.
 # Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.
 
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+# def is_circular(head):
+#     current = head
+#     while current.next:
+#         current = current.next
+
+#         if current.next == head:
+#             return True
+#     return False
+     
+    
+
+# num1 = Node(1)
+# num2 = Node(2)
+# num3 = Node(3)
+# num1.next = num2
+# num2.next = num3
+# num3.next = num1
+
+# # num1 -> num2 -> num3 -> num1
+# print(is_circular(num1))
+
+# var1 = Node(1)
+# var2 = Node(2)
+# var3 = Node(3)
+# var1.next = var2
+# var2.next = var3
+# var3.next = var1
+# var1 = Node('var1', Node('var2', Node('var3')))
+# # var1 -> var2 -> var3
+# print(is_circular(var1))
+
+# Problem 2: Find Last Node in a Linked List Cycle
+# Given the head of a singly linked list, write a function that returns the last node in the cycle. 
+# If there is no cycle in the linked list, return None.
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
 
-def is_circular(head):
-    current = head
-    while current.next:
-        current = current.next
+def find_last_node_in_cycle(head):
+     current = head
+     seen = set()
+     prev = None
 
-        if current.next == head:
-            return True
-    return False
-     
-    
+     while current.next:
+         prev = current
+         if current not in seen:
+             seen.add(current)
+         else:
+             return current
+         current = current.next
+     return current
+
 
 num1 = Node(1)
 num2 = Node(2)
 num3 = Node(3)
+num4 = Node(4)
 num1.next = num2
 num2.next = num3
-num3.next = num1
+num3.next = num4
+num4.next = num2
 
-# num1 -> num2 -> num3 -> num1
-print(is_circular(num1))
-
-var1 = Node('var1', Node('var2', Node('var3')))
-# var1 -> var2 -> var3
-print(is_circular(var1))
+print(find_last_node_in_cycle(num1))
