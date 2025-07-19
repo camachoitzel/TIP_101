fa757af44139e4d7fad394e0f613974bc4dc5881
camachoitzel <camachoitzel101@gmail.com>
2025-07-19 15:45:59 -0700

    Week 7 Session 2 set 1 work: bin search

--- Week_7/S2_set1.py
+++ Week_7/S2_set1.py
@@ -4,17 +4,39 @@
 # The input string will only contain the characters ( or ). Your solution must be recursive.
 # Evaluate the time and space complexity of your solution.
 
-def is_nested(paren_s):
-	if paren_s == "":
-		return True
-	if paren_s[0] == "(" and paren_s[-1] == ")" and len(paren_s) >=2:
-		return is_nested(paren_s[1:-1])
+# def is_nested(paren_s):
+# 	if paren_s == "":
+# 		return True
+# 	if paren_s[0] == "(" and paren_s[-1] == ")" and len(paren_s) >=2:
+# 		return is_nested(paren_s[1:-1])
 	
-	return False
+# 	return False
 
-print(is_nested("(())"))
+# print(is_nested("(())"))
 
 # Problem 2: How Many 1s
 # Given a sorted list of integers containing only 0s and 1s, count the total number of 1’s in the array in O(log n) time.
 
 def count_ones(lst):
+	left = 0
+	right = len(lst) - 1
+	start =  -1
+
+	while left <= right:
+		mid = (left + right) // 2
+
+		if lst[mid] < 1:
+			left = mid + 1	
+		else:
+			start = mid
+			right = mid - 1
+	
+	if start == -1:
+		return 0
+	
+	count = len(lst) - start
+	return count
+
+lst = [0,0,0,0,1,1,1]
+result = count_ones(lst)
+print(result)
