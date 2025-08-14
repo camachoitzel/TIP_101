29b18c005be715750f79abc14553a6f9825f727b
camachoitzel <camachoitzel101@gmail.com>
2025-08-13 22:23:21 -0700

    More Unit 1 practice

--- Practice_Units/Unit_1_set2.py
+++ Practice_Units/Unit_1_set2.py
@@ -103,12 +103,31 @@ print(tip3)
 # If both player1 and player2 have the same value, print "It's a tie!".
 
 def rock_paper_scissors(player1, player2):
-    if player1 is "rock" and player2 is "scissors":
-        print("Player 1 wins!")
-        
+    lst = ["rock", "paper", "scissors"]
+    if player1 == player2:
+        print("It's a tie!")
+    
+    elif player1 == "rock":
+        if player2 == "paper":
+            print("Player 2 wins!")
+        else:
+            print("Player 1 wins!")
+    
+    elif player1 == "scissors":
+        if player2 == "rock":
+            print("Player 2 wins!")
+        else:
+            print("Player 1 wins!")
+    
 
-rock_paper_scissors(rock, rock)
-rock_paper_scissors(scissors, rock)
-rock_paper_scissors(scissors, paper)
-rock_paper_scissors(rock, paper)
-rock_paper_scissors(paper, rock)
+    else:
+        if player2 == "scissors":
+            print("Player 2 wins!")
+        else:
+            print("Player 1 wins!")
+
+rock_paper_scissors("rock", "rock")
+rock_paper_scissors("scissors", "rock")
+rock_paper_scissors("scissors", "paper")
+rock_paper_scissors("rock", "paper")
+rock_paper_scissors("paper", "rock")
